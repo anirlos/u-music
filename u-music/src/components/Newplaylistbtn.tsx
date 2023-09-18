d4c02a9a1c22d7cbe2a1324c8db88806a28854d9
justkeepgoin <manage06@naver.com>
2023-09-19 02:23:36 +0900

    새 재생목록 버튼 css 스타일링
    - 새 재생목록 버튼 css 스타일링
    - styled-components 사용

--- u-music/src/components/Newplaylistbtn.tsx
+++ u-music/src/components/Newplaylistbtn.tsx
@@ -1,10 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
 interface ButtonProps {
   label: string;
   onClick: () => void;
 }
 
 const Newplaylistbtn: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+  return <CreateNewplaylistBtn onClick={onClick}>{label}</CreateNewplaylistBtn>;
 };
+
+const CreateNewplaylistBtn = styled.button`
+  background-color: #1d1d1d;
+  color: #fff;
+  padding: 0.7rem 3rem;
+  margin: 10px;
+  border: none;
+  border-radius: 5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #383838;
+  }
+`;
 
 export default Newplaylistbtn;
